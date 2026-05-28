310ed10c0bb0a7d5282af64a34cbcdb26ee2f982
Raj Yadav <mr.rajyadav272207@gmail.com>
2026-05-28 16:56:12 +0530

    database connected

--- Config/db.php
+++ Config/db.php
@@ -1,25 +1,29 @@
 <?php
 
-$host = "127.0.0.1";
+$host = "zephyr.proxy.rlwy.net";
+
 $user = "root";
-$pass = "";
-$dbname = "auth_system";
-$port = 3307;
+
+$pass = "uvCNiOAGyOgHJAkBnfMDgAYAZUjwqaLs";
+
+$dbname = "railway";
+
+$port = 29560;
 
 $conn = mysqli_connect(
-    $host,
-    $user,
-    $pass,
-    $dbname,
-    $port
+$host,
+$user,
+$pass,
+$dbname,
+$port
 );
 
 if(!$conn){
 
-    die(
-        "Database Connection Failed: "
-        . mysqli_connect_error()
-    );
+die(
+"Connection Failed: "
+. mysqli_connect_error()
+);
 
 }
 
